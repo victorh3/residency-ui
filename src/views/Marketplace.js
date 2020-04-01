72da634efcfba22801d8fd1cbf50476867be9344
victorh3 <victorh3@live.com>
2020-03-31 21:20:01 -0500

    Call REST API with axios

--- src/views/Marketplace.js
+++ src/views/Marketplace.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import { getMockData } from '../constants';
 import { Card, CardDeck } from '../components';
 
-const Marketplace = () => (
-  <section className="Marketplace">
-    <CardDeck>
-      {getMockData.map((residency, index) => (
-        <Card key={`${index}-${residency.programId}`} residency={residency} />
-      ))}
-    </CardDeck>
-  </section>
-);
+const Marketplace = () => {
+  axios({
+    method: 'get',
+    baseURL: 'https://localhost:5003',
+    url: '/categories',
+  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return (
+    <section className="Marketplace">
+      <CardDeck>
+        {getMockData.map((residency, index) => (
+          <Card key={`${index}-${residency.programId}`} residency={residency} />
+        ))}
+      </CardDeck>
+    </section>
+  );
+};
 
 export default Marketplace;
