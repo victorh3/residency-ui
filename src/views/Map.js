c5a917bb5b93c2167f0522d9e430db20141ee8d8
Tom G <thomas.guarnery@gmail.com>
2020-05-18 21:17:31 -0700

    feature: add map change functionality

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { usePrograms } from '../contexts';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { Card } from '../components';
+import L from 'leaflet';
 
 const MapView = () => {
   const { programs } = usePrograms();
@@ -11,69 +12,77 @@ const MapView = () => {
   const [showModal, setShowModal] = useState(false);
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
-  let defaultZoom = 4;
-  let defaultCenter = [39.82, -98.57];
+  const [bounds, setBounds] = React.useState([32.25233, -110.87966]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-  if (programs.length) {
-    defaultCenter = [
-      programs[0].address.latitude,
-      programs[0].address.longitude,
-    ];
-    defaultZoom = 8;
-  }
+  useEffect(() => {
+    const latLngs = programs.map((program) => {
+      return L.latLng(program.address.latitude, program.address.longitude);
+    });
+
+    const bnds = L.latLngBounds(latLngs);
+    setBounds(bnds);
+    setIsLoading(false);
+  }, [programs]);
 
   return (
-    <Map center={defaultCenter} zoom={defaultZoom}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {programs.map((program) => (
-        <Marker
-          key={program.programId}
-          position={[program.address.latitude, program.address.longitude]}
-          onClick={() => {
-            setSelectedProgram(program);
-          }}
-        />
-      ))}
-      {selectedProgram && (
-        <Popup
-          position={[
-            selectedProgram.address.latitude,
-            selectedProgram.address.longitude,
-          ]}
-          onClose={() => {
-            setSelectedProgram(null);
-          }}
-        >
-          <h6>{selectedProgram.programName}</h6>
-          <div>{selectedProgram.address.addressLineOne}</div>
-          <div>{selectedProgram.address.addressLineTwo}</div>
-          <div>{`${selectedProgram.address.city}, ${selectedProgram.address.state} ${selectedProgram.address.zipCode} `}</div>
-          <Button
-            className="Map-ShowModal--button"
-            variant="link"
-            onClick={handleShow}
+    <div>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <Map bounds={bounds}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {programs.map((program) => (
+            <Marker
+              key={program.programId}
+              position={[program.address.latitude, program.address.longitude]}
+              onClick={() => {
+                setSelectedProgram(program);
+              }}
+            />
+          ))}
+          {selectedProgram && (
+            <Popup
+              position={[
+                selectedProgram.address.latitude,
+                selectedProgram.address.longitude,
+              ]}
+              onClose={() => {
+                setSelectedProgram(null);
+              }}
+            >
+              <h6>{selectedProgram.programName}</h6>
+              <div>{selectedProgram.address.addressLineOne}</div>
+              <div>{selectedProgram.address.addressLineTwo}</div>
+              <div>{`${selectedProgram.address.city}, ${selectedProgram.address.state} ${selectedProgram.address.zipCode} `}</div>
+              <Button
+                className="Map-ShowModal--button"
+                variant="link"
+                onClick={handleShow}
+              >
+                Learn more
+              </Button>
+            </Popup>
+          )}
+          <Modal
+            show={showModal}
+            onHide={handleClose}
+            size="lg"
+            aria-labelledby="modal-residency-map-location"
+            centered
           >
-            Learn more
-          </Button>
-        </Popup>
+            <Card
+              program={selectedProgram}
+              style={{ margin: '0' }}
+              className="Map-ModalCard"
+            />
+          </Modal>
+        </Map>
       )}
-      <Modal
-        show={showModal}
-        onHide={handleClose}
-        size="lg"
-        aria-labelledby="modal-residency-map-location"
-        centered
-      >
-        <Card
-          program={selectedProgram}
-          style={{ margin: '0' }}
-          className="Map-ModalCard"
-        />
-      </Modal>
-    </Map>
+    </div>
   );
 };
 
